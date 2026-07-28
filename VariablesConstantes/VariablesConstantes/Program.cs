76f1009549257f76c69061320411e63b575a7962
B09S104est <B09S104est@MEDAULAS.UPB>
2026-07-28 11:16:43 -0500

    Este es mi primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre="Samuel Arango";
+            Console.WriteLine("Bienvenido "+ nombre +" al curso de fundamentos de programación");
+        }
+    }
+}
