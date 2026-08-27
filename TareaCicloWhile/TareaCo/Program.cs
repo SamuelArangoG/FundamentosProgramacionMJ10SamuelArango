86905872b2ace0fd291f1b2542c6dd03adaaf82c
SamuelArangoG <samuel.arangog@upb.edu.co>
2026-08-26 19:49:30 -0500

    TareaCicloWhile

---
+++ TareaCicloWhile/TareaCo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TareaCo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+            int número = 0;
+
+            Console.WriteLine("Bienvenido, digite los números que quiere sumar y cierre la secuencia escribiendo un número negativo");
+            while (número >= 0)
+            {
+                número = int.Parse(Console.ReadLine());
+                acumulador = acumulador + número;
+            }
+            Console.WriteLine($"El resultado de la suma es : {acumulador} ");
+        }
+    }
+}
