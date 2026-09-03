a6a7cc391cd7eb4b94216d969069444197568e98
SamuelArangoG <samuel.arangog@upb.edu.co>
2026-09-03 11:27:11 -0500

    13.CicloFor

---
+++ 13.CicloFor/13.CicloFor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int contador = 1; contador <= 5; contador++)
+            {
+                acumulador += contador;
+            }
+            Console.WriteLine($"La suma de los cinco primeros números es: {acumulador}");
+        }
+    }
+}
