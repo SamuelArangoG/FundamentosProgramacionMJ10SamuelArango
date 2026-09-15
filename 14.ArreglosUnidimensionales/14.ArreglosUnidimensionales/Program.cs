5182a1e3e5dcfb5bdeba795114a7aca2c75a0739
SamuelArangoG <samuel.arangog@upb.edu.co>
2026-09-15 11:33:08 -0500

    14.ArreglosUnidimensionales

---
+++ 14.ArreglosUnidimensionales/14.ArreglosUnidimensionales/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.ArreglosUnidimensionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*//Arregos unidimensionales - Vectores
+            int[] numeros = new int[5];
+            numeros[0] = 15;
+            numeros[1] = 102;
+            numeros[2] = 54;
+            numeros[3] = 26;
+            numeros[4] = 5;
+            //numeros[5] = 11; No se puede orque la posición 6 con índice 5 no xiste.
+            Console.WriteLine($"El número almacenado en la posición 4 on índice 3 es: {numeros[3]}");
+            //Otras formas de declarar e inicializar vectores
+            char[] simbolos = new char[] {'?','/','o','5'};
+            bool[] valoresVerdad = { true, false, true, false, true, true };
+            //Recorrer para llenar de datos el vector
+            string[] nombres = new string[7];
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre para la P{i + 1}:I{i}:");
+                nombres[i] = Console.ReadLine();
+            }
+            //Recorrer para recuperar datos almacenados
+            for(int i=0;i<nombres.Length;i++)
+            {
+                Console.Write($"{nombres[i]} |");
+            }*/
+            //1.Crear arreglo llamado "enteros" de 100 elementos asignar el número 10 en cada una de las posiciones del arreglo. Leer el contenido de cada elemento y mostrarlo en pantalla.
+
+
+            //2.Algoritmo que permita solicitar 10 números, los cuales serán almacenados en un arreglo, al final, debe visualizar el promedio de esos números.
+            /*int[] numeros = new int[10];
+            int contador = 0;
+            Console.WriteLine("Digite los 10 números para luego poder ver su promedio");
+            for (int i = 0; i <= 9; i++)
+            {
+                numeros[i] =int.Parse(Console.ReadLine());
+                contador += numeros[i];
+            }
+            Console.WriteLine($"El promedio de los 10 números es de {contador / 10}");*/
+
+
+            //3.Llene un arreglo con 15 números ingresados por teclado. Unavez registrado el total de vaores, muestre en pantalla todos los elementos del arreglo.
+            //Finalmente,determine cuál es el número mayor y cuál es el número menor, junto con la posición que ocupa cada uno dentro del arreglo.
+            int[] numero = new int[15];
+
+
+
+
+        }
+    }
+
+}
